08176e585397ae6ad67f64b4c5ea6c482d70dc77
Peter Gehlert Theilgaard <58323386+thepanda1711@users.noreply.github.com>
2020-04-08 11:24:49 +0200

    Oprettelse af Patient Interface klassen

---
+++ RPI_Software/Patient_Interface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPI_Software
+{
+    class Patient_Interface
+    {
+        public void ShowMenu()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void CountDown10()
+        { }
+
+        public void CountDown50()
+        { }
+
+        public void ReadingDone()
+        { }
+
+
+    }
+}
