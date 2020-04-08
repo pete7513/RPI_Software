1da098a120d32dc7ca6682ac32f55519429255ba
Peter Gehlert Theilgaard <58323386+thepanda1711@users.noreply.github.com>
2020-04-08 11:52:55 +0200

    Beskrivelse af metoder i klassen Patient interface

--- RPI_Software/Patient_Interface.cs
+++ RPI_Software/Patient_Interface.cs
@@ -8,22 +8,31 @@ namespace RPI_Software
     {
         public void ShowMenu()
         {
-
+            // Når programmet starter skal denne metode kaldes, den skal tænde display og vise de forskellige menuer på det.
         }
 
         public void Read()
         {
-
+            // Denne metode skal registrere hvilken skærm type der er på display og aflæse denne, hvorefter den aktiverer den valgte metode
+            // til det der står på displayet
         }
 
-        public void CountDown10()
-        { }
+        public int CountDown10()
+        { 
+            // Denne metode er en nedtællingsmetode der skal få displayet til at at indikere at en måling er startet, og brugeren har 10
+            // sekunder til at gøre sig klar
+        }
 
-        public void CountDown50()
-        { }
+        public int CountDown50()
+        {
+            // Denne metode er en nedtællingsmetode der skal få displayet til at at indikere at en måling er igang, og brugeren har 10
+            // sekunder til at gøre sig klar
+        }
 
         public void ReadingDone()
-        { }
+        { 
+            // Denne metode skal få displayet til at indikere at måligen er slut.
+        }
 
 
     }
