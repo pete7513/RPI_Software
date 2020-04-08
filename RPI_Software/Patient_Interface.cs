9eb78fb9208600d02cbf66caaeb82d84aa5e2151
Peter Gehlert Theilgaard <58323386+thepanda1711@users.noreply.github.com>
2020-04-08 12:13:29 +0200

    ShowMenu() er påbegyndt

--- RPI_Software/Patient_Interface.cs
+++ RPI_Software/Patient_Interface.cs
@@ -1,20 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RaspberryPiCore.LCD;
+using RaspberryPiCore.TWIST;
+using System.Threading;
 
 namespace RPI_Software
 {
     class Patient_Interface
     {
+        SerLCD serLCD = new SerLCD();
+        TWIST twist = new TWIST();
+
         public void ShowMenu()
         {
             // Når programmet starter skal denne metode kaldes, den skal tænde display og vise de forskellige menuer på det.
+            serLCD.lcdDisplay();
+            serLCD.lcdPrint("Velkommen - programmet starter");
+            Thread.Sleep(5000);
+            serLCD.lcdClear();
+            serLCD.lcdPrint("Start EKG-måling");
         }
 
         public void Read()
         {
             // Denne metode skal registrere hvilken skærm type der er på display og aflæse denne, hvorefter den aktiverer den valgte metode
             // til det der står på displayet
+            string screen = "Start EKG-måling";
+            if (twist.isPressed() == true)
+            {
+
+            }
+            
         }
 
         public int CountDown10()
